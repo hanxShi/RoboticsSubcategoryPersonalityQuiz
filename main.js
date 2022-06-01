f162659354f2b97b5a52bb79d649ac581aef9a8b
hanxShi <94000777+hanxShi@users.noreply.github.com>
2022-06-01 09:01:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // Global Variables
 // Categories are stored in an array
 // Alphabetical order
-var electricalIndex = 0;
-var hardwareIndex = 1;
-var marketingIndex = 2;
-var outreachIndex = 3;
-var safetyIndex = 4;
-var scoutingIndex = 5;
-var softwareIndex = 6;
+const electricalIndex = 0;
+const hardwareIndex = 1;
+const marketingIndex = 2;
+const outreachIndex = 3;
+const safetyIndex = 4;
+const scoutingIndex = 5;
+const softwareIndex = 6;
 
 
 var categories = new Array(0, 0, 0, 0, 0, 0, 0);
+
+var currentQuestion = 0;
 
