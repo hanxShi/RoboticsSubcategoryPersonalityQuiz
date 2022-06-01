250aa7bd411cb8702ac69b88a94fc58d99dfccb4
hanxShi <94000777+hanxShi@users.noreply.github.com>
2022-06-01 09:11:49 -0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,86 @@
+const questions = {
+  { 
+    //Question 1
+    "question" : "The robot is on fire. What do you do?",
+    "answers" : ["Not my problem. I will never have to deal with something like this.", 
+                 "Take out your phone to video the entire scene.", 
+                 "Scream in terror and run as far away as you possibly can.",
+                 "Help calm everybody down.",
+                  "Throw something at it. Like a mallet.",
+                  "Turn the robot off.",
+                  "Find the nearest fire extinguisher."]
+  },
+  {
+    // Question 2
+    "question" : "What is the worst thing you have done out of the below (if any)?",
+    "answer1" : "",
+    "answer2" : "",
+    "answer3" : "",
+    "answer4" : "",
+  },
+  {
+    // Question 3
+    "question" : "",
+    "answer1" : "",
+    "answer2" : "",
+    "answer3" : "",
+    "answer4" : "",
+  },
+  {
+    // Question 4
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 5
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 6
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 7
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 8
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 9
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  {
+    // Question 10
+    "question" : ,
+    "answer1" : ,
+    "answer2" : ,
+    "answer3" : ,
+    "answer4" : ,
+  },
+  
+}
