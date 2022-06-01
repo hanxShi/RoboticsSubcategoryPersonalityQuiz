063b16e6b77637934f231e248653e62379476790
hanxShi <94000777+hanxShi@users.noreply.github.com>
2022-06-01 09:18:02 -0600

    Create main.js

--- main.js
+++ main.js
@@ -14,3 +14,72 @@ var categories = new Array(0, 0, 0, 0, 0, 0, 0);
 
 var currentQuestion = 0;
 
+const container = document.querySelector('.quiz-container');
+const questionEl = document.querySelector('.question');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const nextButton = document.querySelector('.next'); 
+
+
+//Function to generate question 
+function generateQuestions (index) {
+  
+    //Select each question by passing it a particular index
+    const question = questions[index];
+    //Populate html elements 
+    questionEl.innerHTML = `${index + 1}. ${question.question}`
+    option1.innerHTML = `${question.answer1}`
+    option2.innerHTML = `${question.answer2}`
+    option3.innerHTML = `${question.answer3}`
+}
+
+function loadNextQuestion () {
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    //Check if there is a radio input checked
+    if(!selectedOption) {
+        alert('Please select your answer!');
+        return;
+    }
+    //Get value of selected radio
+    const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
+
+    ////Add the answer score to the score array
+    score.push(answerScore);
+
+    selectedAnswersData.push()
+    
+
+    const totalScore = score.reduce((total, currentNum) => total + currentNum);
+
+    //Finally we incement the current question number ( to be used as the index for each array)
+    currentQuestion++;
+
+        //once finished clear checked
+        selectedOption.checked = false;
+    //If quiz is on the final question
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Finish';
+    }
+    //If the quiz is finished then we hide the questions container and show the results 
+    if(currentQuestion == totalQuestions) {
+        container.style.display = 'none';
+        result.innerHTML =
+         `<h1 class="final-score">Your score: ${totalScore}</h1>
+         <div class="summary">
+            <h1>Summary</h1>
+            <p>Possible - Personality Traits, see below for a summary based on your results:</p>
+            <p>15 - 21- You Need Help</p>
+            <p>10 - 15 - Good Soul</p>
+            <p>5 - 10 - Meh </p>
+            <p>5 - Are You Even Real</p>
+        </div>
+        <button class="restart">Restart Quiz</button>
+         `;
+        return;
+    }
+    generateQuestions(currentQuestion);
+}
+
+nextButton.addEventListener('click', loadNextQuestion);
+result.addEventListener('click', restartQuiz);
