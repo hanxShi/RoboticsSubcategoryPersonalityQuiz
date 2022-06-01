3f20cf72ac28dfcc0baf85401696ac7fa9d6555d
hanxShi <94000777+hanxShi@users.noreply.github.com>
2022-06-01 08:54:09 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Global Variables
+// Categories are stored in an array
+// Alphabetical order
+var electricalIndex = 0;
+var hardwareIndex = 1;
+var marketingIndex = 2;
+var outreachIndex = 3;
+var safetyIndex = 4;
+var scoutingIndex = 5;
+var softwareIndex = 6;
+
+
+var categories = new Array(0, 0, 0, 0, 0, 0, 0);
+
